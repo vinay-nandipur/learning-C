734c268876c395b66ec814ce374c015c786f4159
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-01-29 14:10:56 +0530

    comments added for prgm1.c

--- prgm1.c
+++ prgm1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+// #include -> Directive
+// <stdio.h> -> Preprocessor (file ending with *.h is an header file)
 int main(void)
+// Every C Program starts with function name manin with open/close parenthesis()
+// void in main function means with no arguments/parameters
 {
+	// Main function body starts and ends with curly braces
 	puts("Let's Learn C Programming");
+	// puts is an function retrieved from stdio.h header file
 	return 0;
+	// return tells the exit status of main function to the OS
 }
