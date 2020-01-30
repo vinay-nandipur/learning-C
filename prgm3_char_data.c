252f2de2ee0dab5a5d1499a9399540ecbb280a96
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-01-30 15:52:59 +0530

    updated prgm3_char_data.c

--- prgm3_char_data.c
+++ prgm3_char_data.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 int main(void) {
      int VarInt;
      char VarChar;
@@ -13,12 +14,18 @@ int main(void) {
      printf("\tInteger Value: %d\n \tASCII Hexadecimal Value: %x\n \tASCII Octal Value: %o\n",i,i,i);
 
      int value,square;
+     float squareroot;
      printf("\tGive me a number and I will square it!\n");
      scanf("%d", &value);
      //scanf is a function retrieved from stdio.h header file used to read user input, & symbol used to identify memory location of variable
+     if(value >= 0.0) {
+        squareroot = sqrt(value);
+        //sqrt function retrieved from math.h header file, used to find squareroot.
+      }
      square = value * value;
      printf("\tYou've given %d\n",value);
      printf("\tThe squared value is %d\n",square);
+     printf("The sqaure root is: %f\n",squareroot);
 
 
      return 0;
