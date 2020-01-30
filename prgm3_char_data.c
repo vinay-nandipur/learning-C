990445af5ca77d1d1fa688a3030140f27892d3eb
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-01-30 15:31:38 +0530

    updated prgm3_char_data.c

--- prgm3_char_data.c
+++ prgm3_char_data.c
@@ -7,5 +7,8 @@ int main(void) {
      VarChar = 'r';
      VarFloat = 3.1415;
      printf("\tThe year is %d.\n \tThe radius is denoted as %c\n \twhile PI is equal to %f\n",VarInt, VarChar, VarFloat);
+     int i;
+     i = 31;
+     printf("\tInteger Value: %d\n \tASCII Hexadecimal Value: %x\n \tASCII Octal Value: %o\n",i,i,i);
      return 0;
 }
