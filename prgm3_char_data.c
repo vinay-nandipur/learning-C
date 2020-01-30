ec9380ed4e6c8e5cdef85c3c57eca5993f5e13bb
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-01-30 15:21:36 +0530

    prgm3_char_data.c added to print some char data

---
+++ prgm3_char_data.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+     int VarInt;
+     char VarChar;
+     float VarFloat;
+     VarInt = 2012;
+     VarChar = 'r';
+     VarFloat = 3.1415;
+     printf("\tThe year is %d.\n \tThe radius is denoted as %c\n \twhile PI is equal to %f\n",VarInt, VarChar, VarFloat);
+     return 0;
+}
