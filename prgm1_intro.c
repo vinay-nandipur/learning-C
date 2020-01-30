2d8aaa367ae7b27988b80cf10c50715378a52dc8
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-01-30 14:22:16 +0530

    prgm1_basic_math.c for basic maths

---
+++ prgm1_intro.c
@@ -0,0 +1,36 @@
+/*
+This example program will do basic maths for you
+*/
+#include <stdio.h>
+// #include -> Directive
+// <stdio.h> -> Preprocessor (file ending with *.h is an header file)
+int main(void)
+// Every C Program starts with function name main with open/close parenthesis()
+// void in main function means with no arguments/parameters
+{
+	// Main function body starts and ends with curly braces
+	puts("Starting Basic Maths");
+	// puts is an function retrieved from stdio.h header file
+	int var1;
+	// int keyword to define integer data type
+	var1 = 100;
+	var1 = var1 + 100;
+	printf("Value of var1 variable is: %d\n", var1);
+
+	int var2 = 50;
+	float var3;
+  // float keyword to define Float data type
+  var3 = var1 - var2;
+  printf("Value of var3 variable is: %f\n", var3);
+
+	int i,j,k,l;
+	i=100;
+	j=25;
+	k=13;
+	l=(5*((j%k)+i)/(2*k))/2;
+
+	printf("Value of l variable is: %d\n", l);
+
+	return 0;
+	// return tells the exit status of main function to the OS
+}
