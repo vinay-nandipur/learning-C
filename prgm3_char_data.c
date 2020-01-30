5869ccc2f5504cb33cf012c2dcd5ee1dedb8c5f4
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-01-30 15:49:10 +0530

    prgm3_char_data.c updated

--- prgm3_char_data.c
+++ prgm3_char_data.c
@@ -7,8 +7,19 @@ int main(void) {
      VarChar = 'r';
      VarFloat = 3.1415;
      printf("\tThe year is %d.\n \tThe radius is denoted as %c\n \twhile PI is equal to %f\n",VarInt, VarChar, VarFloat);
+
      int i;
      i = 31;
      printf("\tInteger Value: %d\n \tASCII Hexadecimal Value: %x\n \tASCII Octal Value: %o\n",i,i,i);
+
+     int value,square;
+     printf("\tGive me a number and I will square it!\n");
+     scanf("%d", &value);
+     //scanf is a function retrieved from stdio.h header file used to read user input, & symbol used to identify memory location of variable
+     square = value * value;
+     printf("\tYou've given %d\n",value);
+     printf("\tThe squared value is %d\n",square);
+
+
      return 0;
 }
