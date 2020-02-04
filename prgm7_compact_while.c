119e5768d87a70dfce5045064b11f3f8be4c0b43
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-02-04 12:25:35 +0530

    prgm7_compact_while.c added for compact while loop

---
+++ prgm7_compact_while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int counter = 5;
+
+    while(counter != 0) {
+        puts("I am an awesome compact program");
+        counter--;
+    }
+    return 0;
+}
