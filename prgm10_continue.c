3d9e92e91e66e3ceb6c9ec6d5f5cfa710e07aa8b
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-02-04 15:01:25 +0530

    prgm10_continue.c & prgm11_break.c added

---
+++ prgm10_continue.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    int number;
+    int max = 0;
+    int counter = 3;
+
+    do {
+        printf("Enter the Number:\n");
+        scanf("%d",&number);
+        if(number <= 100 && number >= 0)
+        {
+            counter--;
+            if(number > max)
+                max = number;
+        }
+        else
+            {
+                printf("entered number is invalid!, number should be betweeen 0 to 100\n");
+
+            }
+        continue;
+
+
+    } while (counter != 0);
+
+          printf("The largest number is %d\n",max);
+
+    return 0;
+}
