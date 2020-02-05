21be220c3df9863d654abe6e59451489136719d6
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-02-05 14:04:57 +0530

    prgm13_test_operators.c & prgm14_long_test.c added

---
+++ prgm13_test_operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i;
+  i = 100;
+     if(i >= 100) {
+        i += 100;
+        if(i >= 0)
+           i -= 100;
+        else {
+          i += 100;
+        }
+        i *= 5;
+      }
+     else
+        i -= 100;
+    printf("%d", i);
+    return 0;
+
+}
---
+++ prgm14_long_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  long l;
+  int i;
+  l = 1l + 1L;
+  i = l;
+  printf("%d",i);
+  return 0;
+}
